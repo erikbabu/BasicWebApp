326aa19692d0eb68b58cc7deb01bdb0eb2c76c51
Erik Babu <eb1816@ic.ac.uk>
2020-01-14 19:47:26 +0000

    Add failing test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void knowsAboutErik() throws Exception {
+        assertThat(queryProcessor.process("Erik"), containsString("deploy"));
+    }
 }
