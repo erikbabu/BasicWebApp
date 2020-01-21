4d81f629412300889d6fd9d2ba99580df7b6a148
g-ongetta <georgeongetta@gmail.com>
2020-01-21 12:07:39 +0000

    Trycatch

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -17,79 +17,84 @@ public class QueryProcessor {
     }
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                "English poet, playwright, and actor, widely regarded as the greatest " +
-                "writer in the English language and the world's pre-eminent dramatist.";
-        }
-
-        if (query.toLowerCase().contains("what is your team name")) {
-            return "The LADS️";
-        }
-
-        if (query.toLowerCase().contains("erik")) {
-            return "Erik Babu (23 October 1996 - Present) is an awesome " +
-                "DevOps engineer with a keen eye on how to deploy.";
-        }
-
-        if (query.toLowerCase().contains("plus")) {
-            String[] words = query.split(" ");
-            return String.valueOf(Integer.parseInt(words[2]) + Integer.parseInt(words[4]));
-        }
-
-        if (query.toLowerCase().contains("multiplied")) {
-            String[] words = query.split(" ");
-            return String.valueOf(Integer.parseInt(words[2]) * Integer.parseInt(words[5]));
-        }
-
-
-        if (query.toLowerCase().contains("numbers is the largest")) {
-            String queryLower = query.toLowerCase();
-            String numbers = queryLower.substring(query.lastIndexOf(':') + 1);
-            Integer maximum = Arrays.asList(numbers.split(","))
-                                    .stream()
-                                    .map(String::trim)
-                                    .mapToInt(Integer::parseInt)
-                                    .max()
-                                    .getAsInt();
-            return maximum.toString();
-        }
-
-
-        if (query.toLowerCase().contains("both a square and a cube")) {
-            String queryLower = query.toLowerCase();
-            String numbers = queryLower.substring(query.lastIndexOf(':') + 1);
-            int[] maximum = Arrays.asList(numbers.split(","))
-                .stream()
-                .map(String::trim)
-                .mapToInt(Integer::parseInt)
-                .filter(this::isSquareAndCube)
-                .toArray();
-
-            return Arrays.toString(maximum);
-        }
-
-        if (query.toLowerCase().contains("are prime")) {
-            String queryLower = query.toLowerCase();
-            String numbers = queryLower.substring(query.lastIndexOf(':') + 1);
-            int[] primes = Arrays.stream(numbers.split(","))
-                .map(String::trim)
-                .mapToInt(Integer::parseInt)
-                .filter(this::isPrime)
-                .toArray();
-
-            return Arrays.toString(primes);
-        }
-
-        if (query.toLowerCase().contains("sequence")) {
-            int n = Integer.parseInt(query.replaceAll("[^-?0-9]+", " ").trim());
-            return String.valueOf(fibonacci(n));
-        }
-
-        for (String key: word_assoc.keySet()) {
-            if (query.toLowerCase().contains(key)) {
-                return word_assoc.get(key);
+        try {
+            if (query.toLowerCase().contains("shakespeare")) {
+                return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                    "English poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the English language and the world's pre-eminent dramatist.";
             }
+
+            if (query.toLowerCase().contains("what is your team name")) {
+                return "The LADS️";
+            }
+
+            if (query.toLowerCase().contains("erik")) {
+                return "Erik Babu (23 October 1996 - Present) is an awesome " +
+                    "DevOps engineer with a keen eye on how to deploy.";
+            }
+
+            if (query.toLowerCase().contains("plus")) {
+                String[] words = query.split(" ");
+                return String.valueOf(Integer.parseInt(words[2]) + Integer.parseInt(words[4]));
+            }
+
+            if (query.toLowerCase().contains("multiplied")) {
+                String[] words = query.split(" ");
+                return String.valueOf(Integer.parseInt(words[2]) * Integer.parseInt(words[5]));
+            }
+
+
+            if (query.toLowerCase().contains("numbers is the largest")) {
+                String queryLower = query.toLowerCase();
+                String numbers = queryLower.substring(query.lastIndexOf(':') + 1);
+                Integer maximum = Arrays.asList(numbers.split(","))
+                    .stream()
+                    .map(String::trim)
+                    .mapToInt(Integer::parseInt)
+                    .max()
+                    .getAsInt();
+                return maximum.toString();
+            }
+
+
+            if (query.toLowerCase().contains("both a square and a cube")) {
+                String queryLower = query.toLowerCase();
+                String numbers = queryLower.substring(query.lastIndexOf(':') + 1);
+                int[] maximum = Arrays.asList(numbers.split(","))
+                    .stream()
+                    .map(String::trim)
+                    .mapToInt(Integer::parseInt)
+                    .filter(this::isSquareAndCube)
+                    .toArray();
+
+                return Arrays.toString(maximum);
+            }
+
+            if (query.toLowerCase().contains("are prime")) {
+                String queryLower = query.toLowerCase();
+                String numbers = queryLower.substring(query.lastIndexOf(':') + 1);
+                int[] primes = Arrays.stream(numbers.split(","))
+                    .map(String::trim)
+                    .mapToInt(Integer::parseInt)
+                    .filter(this::isPrime)
+                    .toArray();
+
+                return Arrays.toString(primes);
+            }
+
+            if (query.toLowerCase().contains("sequence")) {
+                int n = Integer.parseInt(query.replaceAll("[^-?0-9]+", " ").trim());
+                return String.valueOf(fibonacci(n));
+            }
+
+            for (String key : word_assoc.keySet()) {
+                if (query.toLowerCase().contains(key)) {
+                    return word_assoc.get(key);
+                }
+            }
+        } catch (Exception e)
+        {
+            return "";
         }
 
         return "";
