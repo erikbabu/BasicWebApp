9cce60da0cff8faff5ac2875f571074522243b4b
g-ongetta <georgeongetta@gmail.com>
2020-01-21 11:19:13 +0000

    Team name query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,9 +5,15 @@ public class QueryProcessor {
     public String process(String query) {
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+                "English poet, playwright, and actor, widely regarded as the greatest " +
+                "writer in the English language and the world's pre-eminent dramatist.";
         }
+
+        if (query.toLowerCase().contains("what is your team name")) {
+            return "The LADS™️";
+        }
+
         return "";
     }
+
 }
